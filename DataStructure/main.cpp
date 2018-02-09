d968f21e331fff029de5e68e032a344ab66bbb0c
Ryo Ryo <qq250707340@163.com>
2018-02-10 01:31:10 +0800

    Add new learning project.

---
+++ DataStructure/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n;//由于作用域，n的声明不能放在循环内
+	do {
+		n = 0;
+		cout << "Input a number to run program(s):" << endl;
+		cout << "1:Programs of vector" << endl;
+		cout << "If you input 0 or others, this program will be ended." << endl;
+		cin >> n;
+		switch (n) {
+		case 1:
+			cout << "Program1 is running." << endl;
+			break;
+		default:
+			cout << "Program end." << endl;
+			n = 0;
+		}
+	} while (n);
+}
