46fe932adfbb0aef5db4519d00cb5cd5f9493955
RyoJerryYu <tzl19961216@gmail.com>
2018-02-24 21:13:01 +0800

    add

--- pattest/main.cpp
+++ pattest/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "head.h"
 using namespace std;
 int main() {
+	int n=1;
+	while (n) {
+		cout << "input a number below:" << endl;
+		cout << "1:1016" << endl;
+		cout << "input others to exit." << endl;
+		cin >> n;
+		switch (n) {
+		case 1:
+			pro1016();
+			break;
+		default:
+			n = 0;
+		}
+	}
+	
 	return 0;
 }
