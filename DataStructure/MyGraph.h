21194b914440cf8eb2a993881bb82049b2a48041
RyoJerryYu <tzl19961216@gmail.com>
2018-03-25 13:38:04 +0800

    add myGraph.

---
+++ DataStructure/MyGraph.h
@@ -0,0 +1,12 @@
+#ifndef MY_GRAPH_H
+#define MY_GRAPH_H
+
+#include <iostream>
+using namespace std;
+
+typedef enum { UNDISCOVERD, DISCOVERED, VISITED } VStatus;
+template <typename Tv> struct MyVertex {
+
+};
+
+#endif //MY_GRAPH_H
